6f9962d23c7508ad25b9f6c79b8bd5063f90358c
kunduanushree <anushreekundu04@gmail.com>
2025-07-20 20:01:07 +0530

    Add files via upload

---
+++ scd_type_6_procedure.sql
@@ -0,0 +1,21 @@
+DELIMITER //
+CREATE PROCEDURE scd_type_6()
+BEGIN
+    UPDATE dim_customer d
+    JOIN stg_customer s ON d.customer_id = s.customer_id
+    SET d.end_date = CURDATE(), d.current_flag = 0
+    WHERE d.current_flag = 1 AND (d.name <> s.name OR d.address <> s.address);
+
+    INSERT INTO dim_customer (customer_id, name, address, prev_address, city, state, zip, effective_date, end_date, current_flag, version)
+    SELECT s.customer_id, s.name, s.address, d.address, s.city, s.state, s.zip, CURDATE(), NULL, 1, COALESCE(d.version, 0) + 1
+    FROM stg_customer s
+    JOIN dim_customer d ON s.customer_id = d.customer_id
+    WHERE d.current_flag = 1 AND (d.name <> s.name OR d.address <> s.address);
+
+    INSERT INTO dim_customer (customer_id, name, address, prev_address, city, state, zip, effective_date, end_date, current_flag, version)
+    SELECT s.customer_id, s.name, s.address, NULL, s.city, s.state, s.zip, CURDATE(), NULL, 1, 1
+    FROM stg_customer s
+    LEFT JOIN dim_customer d ON s.customer_id = d.customer_id
+    WHERE d.customer_id IS NULL;
+END //
+DELIMITER;
